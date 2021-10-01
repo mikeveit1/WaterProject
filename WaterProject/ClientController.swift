00fc015d2334ddf88e39876bed820456a7dbb488
Michael Veit <mikeveit1@gmail.com>
2021-10-01 16:37:09 -0400

    added table view extension

---
+++ WaterProject/ClientController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  WaterProject
+//
+//  Created by Michael Veit on 10/1/21.
+//
+
+import UIKit
+import Foundation
+
+class ClientController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
+extension ClientController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        return 
+    }
+    
+    
+}
