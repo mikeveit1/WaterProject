f06a5e89f555635425a3cb5c301dc6b8aadab5ba
Michael Veit <mikeveit1@gmail.com>
2021-10-01 21:24:12 -0400

    optimized currency formatters

--- WaterProject/Formatters/CurrencyFormatter.swift
+++ WaterProject/Formatters/CurrencyFormatter.swift
@@ -9,29 +9,40 @@ import Foundation
 import UIKit
 
 
+private var numberFormatter = NumberFormatter()
+internal var currencyPlaceholder = "$0.00"
+
 public func formatStringToCurrency(amount: Double) -> String {
-    let numberFormatter = NumberFormatter()
+    numberFormatter = NumberFormatter()
     numberFormatter.numberStyle = .currency
-    let formattedString = numberFormatter.string(from: NSNumber(value: amount)) ?? "$0.00"
+    let formattedString = numberFormatter.string(from: NSNumber(value: amount)) ?? currencyPlaceholder
     return formattedString
 }
 
-extension String {
-    public func formatStringInputToCurrency() -> String {
-        var number: NSNumber!
-        let formatter = NumberFormatter()
-        formatter.numberStyle = .currencyAccounting
-        formatter.currencySymbol = "$"
-        formatter.maximumFractionDigits = 2
-        formatter.minimumFractionDigits = 2
-        var amountWithPrefix = self
-        let regex = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
-        amountWithPrefix = regex.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, self.count), withTemplate: "")
-        let double = (amountWithPrefix as NSString).doubleValue
-        number = NSNumber(value: (double / 100))
-        guard number != 0 as NSNumber else {
-            return ""
-        }
-        return formatter.string(from: number)!
+public func getDoubleFromCurrencyString(currencyString: String) -> Double {
+    var amount: Double = 0
+    numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .currency
+    if let number = numberFormatter.number(from: currencyString) {
+        amount = number.doubleValue
     }
+    return amount
+}
+
+public func formatStringInputToCurrency(input: String) -> String {
+    var number: NSNumber!
+    let numberFormatter = NumberFormatter()
+    numberFormatter.numberStyle = .currencyAccounting
+    numberFormatter.currencySymbol = "$"
+    numberFormatter.maximumFractionDigits = 2
+    numberFormatter.minimumFractionDigits = 2
+    var amountWithPrefix = input
+    let expression = try! NSRegularExpression(pattern: "[^0-9]", options: .caseInsensitive)
+    amountWithPrefix = expression.stringByReplacingMatches(in: amountWithPrefix, options: NSRegularExpression.MatchingOptions(rawValue: 0), range: NSMakeRange(0, input.count), withTemplate: "")
+    let double = (amountWithPrefix as NSString).doubleValue
+    number = NSNumber(value: (double / 100))
+    guard number != 0 as NSNumber else {
+        return ""
+    }
+    return numberFormatter.string(from: number)!
 }
